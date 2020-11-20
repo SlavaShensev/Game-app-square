c51a2b5fd131a15ba22688939fa0c1d7636b5413
Slava <SlavaShensev@gmail.com>
2020-11-20 14:38:42 +0200

    'create-square'

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let $start = document.querySelector('#start');
 let $game = document.querySelector('#game');
 
-$start.addEventListener('click', startGame);
+let score = 0;
 
+$start.addEventListener('click', startGame);
+$game.addEventListener('click', handleBoxClick);
 
 function startGame() {
     $game.style.backgroundColor = '#fff';
@@ -11,7 +13,28 @@ function startGame() {
     renderBox()
 }
 
+function handleBoxClick(event) {
+    if(event.target.dataset) {
+        score++;
+        renderBox()
+    }
+}
+
 function renderBox() {
-    
+    $game.innerHTML = '';
+    let box = document.createElement('div');
+
+
+    box.style.height = box.style.width = '50px';
+    box.style.position = 'absolute';
+    box.style.backgroundColor = '#000';
+    box.style.top = '50px';
+    box.style.left = '70px';
+    box.style.cursor = 'pointer';
+    box.setAttribute('data-box', 'true');
+
+
+
+    $game.insertAdjacentElement('afterbegin', box)
 }
 
