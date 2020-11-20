3993e54454cdd603e7c63744d57ceb9c7053903f
Slava <SlavaShensev@gmail.com>
2020-11-20 15:13:10 +0200

    'timer'

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 let $start = document.querySelector('#start');
 let $game = document.querySelector('#game');
+let $time = document.querySelector('#time');
 
 let score = 0;
+let isGameStarted = false;
 
 $start.addEventListener('click', startGame);
 $game.addEventListener('click', handleBoxClick);
 
 function startGame() {
+    isGameStarted = true;
     $game.style.backgroundColor = '#fff';
     $start.classList.add('hide');
 
-    renderBox()
+    let interval = setInterval(function () {
+        let time = parseFloat($time.textContent);
+
+        if (time <= 0) {
+            clearInterval(interval);
+            endGame()
+        } else {
+            $time.textContent = (time - 0.1).toFixed(1);
+        }
+
+    }, 100);
+
+    renderBox();
+}
+
+function endGame() {
+    isGameStarted = false;
 }
 
 function handleBoxClick(event) {
+    if (!isGameStarted) {
+        return
+    }
+
     if (event.target.dataset) {
         score++;
         renderBox()
@@ -32,7 +55,6 @@ function renderBox() {
     let maxTop = gameSize.height - boxSize;
     let maxLeft = gameSize.width - boxSize;
 
-
     box.style.height = box.style.width = boxSize + 'px';
     box.style.position = 'absolute';
     box.style.backgroundColor = '#000';
@@ -40,7 +62,6 @@ function renderBox() {
     box.style.left = getRandom(0, maxLeft) + 'px';
     box.style.cursor = 'pointer';
     box.setAttribute('data-box', 'true');
-
 
     $game.insertAdjacentElement('afterbegin', box)
 }
